2ea72f804f366a701b3bb50460c6e54ba9da9464
Krishna <krishnathescienceguy@outlook.com>
2024-08-01 15:23:12 -0500

    my changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,27 +1,27 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "The Garden",
+  DESCRIPTION: "A manuscript of the disorganized thoughts of Mars Ultor.",
+  EMAIL: "marsultor_carnifex@ayyalasomayajula.net.",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  DESCRIPTION: "The Garden is where some of the more well assembled of my thoughs congregate.",
 };
 
 export const BLOG: Metadata = {
   TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  DESCRIPTION: "Musings of a sleep deprived mind.",
 };
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
   DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos.",
+    "A collection of my projects with links to repositories.",
 };
 
 export const SOCIALS: Socials = [
